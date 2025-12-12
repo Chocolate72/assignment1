98cb4b71f55f86631bd39e08f5d813d70906b0a5
Anuj Basnet <anujbasnet14@gmail.com>
2025-12-12 18:20:13 -0500

    added story parts arrays to the js file

---
+++ script.js
@@ -0,0 +1,7 @@
+// Story parts arrays
+const part1 = ["The funny dog", "A silly robot", "The tiny dinosaur"];
+const part2 = ["jumped over", "ran into", "slid under"];
+const part3 = ["the big fence", "a banana peel", "the red wagon"];
+const part4 = ["because it saw", "while chasing", "after hearing"];
+const part5 = ["a flying cat", "a loud chicken", "a dancing frog"];
+
